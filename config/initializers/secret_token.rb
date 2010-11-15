db1792f6930e546c3530a876dd21a7f9734f8735
Houdini <dmitrii.golub@gmail.com>
2010-11-15 20:01:27 +0300

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvnews::Application.config.secret_token = 'e05559cd6973954f2f6bdb6d0130a02c7ee07ad06120e1148d88d92f4ad5c0739fdee139aa257f1411065b800107017c885cb0575f8029a2bc1bcd0f9b5da6eb'
